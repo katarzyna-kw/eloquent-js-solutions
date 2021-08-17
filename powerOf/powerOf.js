9fedd76f891aca37eb5496195ee1467fc8d992e0
katarzyna_w <katarzyna.koch@gmail.com>
2021-08-17 10:59:26 -0500

    for loop solution

--- powerOf/powerOf.js
+++ powerOf/powerOf.js
@@ -1,8 +1,16 @@
-let result=1
-let counter=0
+// let result=1
+// let counter=0
 
-while (counter<=10) {
+//while loop
+// while (counter<=10) {
+//     console.log(result)
+//     result=result*2
+//     counter++
+// }
+
+//for loop
+let result = 1;
+for (let counter = 0; counter <=10; counter++) {
     console.log(result)
-    result=result*2
-    counter++
+    result = result * 2;
 }
