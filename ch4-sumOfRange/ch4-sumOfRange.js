b8a591ca8743c563b82dfd112d6c0d3ab8cd5042
katarzyna_w <katarzyna.koch@gmail.com>
2021-08-18 15:09:27 -0500

    refactor sumOfRange

--- ch4-sumOfRange/ch4-sumOfRange.js
+++ ch4-sumOfRange/ch4-sumOfRange.js
@@ -1,20 +1,25 @@
 const range = (start, end) => {
     let arr=[]
-    for (let i=start; i<=end; i++) {
-        arr.push(i)
+    if (start<end) {
+        for (let i=start; i<=end; i++) {
+            arr.push(i)
+        }
+    } else {
+        for (let i=start; i>=end; i--) {
+            arr.push(i)
+        }
     }
     return arr;
 }
   
 console.log(range(1, 10))
+console.log(range(5, 2))
 
 const sum = (arr) => {
-//declare answer as 0/data type number
-let answer=0
+    let answer=0
 
-for (let i=0; i<arr.length; i++) {
-    console.log('answer', answer)
-    answer=answer+arr[i]
+    for (let i=0; i<arr.length; i++) {
+        answer+=arr[i]
     }
     return answer
 }
@@ -22,7 +27,6 @@ for (let i=0; i<arr.length; i++) {
 console.log(sum(range(1, 10)))
 
   
-
 const bonusRange = (start, end, step=start<end ? 1 : -1) => {
     let arr=[]
     if (step>0) {
