8e244bbcca06726968a2f49d0ae66475363819c2
katarzyna_w <katarzyna.koch@gmail.com>
2021-08-22 09:46:50 -0500

    refactor fizzbuzz function to return array instead of undefined and test for fizzbuzz

--- fizzbuzz/fizzbuzz.js
+++ fizzbuzz/fizzbuzz.js
@@ -1,16 +1,12 @@
 const fizzbuzz = () => {
+    let answer=[]
     for (let i=1; i<101;i++){
-        if (i%3===0 && i%5===0) {
-            console.log("FizzBuzz")
-        } else if (i%3===0) {
-            console.log("Fizz")
-        } else if (i%5===0){
-            console.log("Buzz")
-        } else {
-        console.log(i)
-        }
+        if (i%3===0 && i%5===0) answer.push("FizzBuzz")
+        else if (i%3===0) answer.push("Fizz")
+        else if (i%5===0) answer.push("Buzz")
+        else answer.push(i)
     }
-    return true
+    return answer
 }
 
-console.log(fizzbuzz());
+module.exports = fizzbuzz
