e8e985335f0974d94b8644458c5a905fd01fdc62
katarzyna_w <katarzyna.koch@gmail.com>
2021-10-14 16:53:29 -0500

    flattening solution for deeply nested arrays

--- ch5-flattening/ch5-flattening.js
+++ ch5-flattening/ch5-flattening.js
@@ -7,10 +7,18 @@ console.log(arrays.reduce((previous, current) => previous.concat(current), [] ))
 
 console.log(arrays.reduce(reducer));
 
-console.log(arr2.reduce((previous, current) => previous.concat(current), []));
+// console.log(arr2.reduce((previous, current) => previous.concat(current), []));
 
 
 //using flat method
 
 console.log(arrays.flat());
 console.log(arr2.flat());
+
+
+//deeply nested cases
+const flattening = (arr) => {
+   return arr.reduce((arr, current) => arr.concat(Array.isArray(current) ? flattening(current) : current), [])
+}
+
+console.log(flattening([1, [2], [3, [[4]]]]));
